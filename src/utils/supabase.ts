49cd26bd2396daacebfc8cf9255c1b768dfccd34
Quan Bui <quanbui021001@gmail.com>
2025-11-20 16:47:56 +0200

    fix: comment dontevn

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL || "http://127.0.0.1:55321";
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
